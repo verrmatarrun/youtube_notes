0b95cf62a681244cb3be746c8eafc4e20bb2abc4
verrmatarrun <tarunverma15sept88@gmail.com>
2023-01-29 21:06:28 +0530

    Added program for replace() method in java

---
+++ java/StringReplace.java
@@ -0,0 +1,12 @@
+class StringReplace
+{
+	public static void main(String args[])
+	{
+		String s1="my name is tarun and i love to cook food and do code. tarun is a good chess player";
+		String s2=s1.replace('t','a');
+
+		System.out.println(s2);
+
+		
+	}
+}
