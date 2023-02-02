b86fa4d63b46d02bb16175b306bf3e18e23e0324
verrmatarrun <tarunverma15sept88@gmail.com>
2023-02-02 10:59:50 +0530

    Java program to convert string to char array

---
+++ java/StringToCharArray.java
@@ -0,0 +1,15 @@
+class StringToCharArray
+{
+	public static void main(String args[])
+	{
+		String s1="ABCDE";
+		char chars[]=s1.toCharArray();
+
+		
+		
+		for(char c:chars)
+			System.out.println(c);
+
+		
+	}
+}
