640d5686663ba875796b186aee6be6bef04506d2
verrmatarrun <tarunverma15sept88@gmail.com>
2023-02-02 18:32:09 +0530

    Added example of trim() method of string class

---
+++ java/StringTrim.java
@@ -0,0 +1,16 @@
+class StringTrim
+{
+	public static void main(String args[])
+	{
+		String s1="   tarun   verma";
+		String s2=s1.trim();
+
+		System.out.println(s1.length());
+		System.out.println(s2.length());
+
+		System.out.println(s1+s2);
+
+
+		
+	}
+}
