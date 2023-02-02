1a61e2d17bd09902b307cc9997e2318dbcb0a9e0
verrmatarrun <tarunverma15sept88@gmail.com>
2023-02-02 11:00:42 +0530

    Added c program to print pattern which require multiple nested loops

---
+++ c/pattern-with-multiple-i-loop.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int i,j;
+	for(i=1;i<=5;i++)
+	{	for(j=1;j<=i;j++)
+		{	printf("%d",j);
+		}
+		printf("\n");
+	}
+	
+	for(i=4;i>=1;i--)
+	{	for(j=1;j<=i;j++)
+		{	printf("%d",j);
+		}
+		printf("\n");
+	}
+	return 0;
+}
