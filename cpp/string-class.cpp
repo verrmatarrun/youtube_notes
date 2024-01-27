5148f80e23deb1c5479f2c79a97400f0aae488c7
verrmatarrun <tarunverma15sept88@gmail.com>
2024-01-27 12:21:24 +0530

    Addes string class in cpp

---
+++ cpp/string-class.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+	
+//	string str;
+//	cout<<"Enter your fullname : ";	
+//	getline(cin,str);
+//	
+//	cout<<"The length of string is "<<str.length();
+
+	string firstname="tarun ",lastname="verma";
+	string fullname=firstname+lastname;
+	cout<<fullname;
+	return 0;
+}
